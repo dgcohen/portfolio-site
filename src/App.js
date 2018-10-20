01ff893a170186d7104df7e7569fa923e93ecd2f
Diego Cohen <diegocohen@Diegos-Macbook-Pro-2.local>
2018-10-20 16:04:15 -0400

    Fix accessibility issues

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
 	            <ul aria-expanded="false" role="navigation" aria-label="Main navigation">
 	              <li><a href="#about">About</a></li>
 	              <li><a href="#projects">Projects</a></li>
-	              <li><a href={resume} target="_blank">Resume</a></li>
+	              <li><a href={resume} target="_blank" rel="noopener noreferrer">Resume</a></li>
 	            </ul>
 	          </div> 
 	    		</div>
@@ -43,8 +43,8 @@ class App extends Component {
 	      		<p>I'm a software engineer based in New York City specializing in React, Node.js,
 	      		 Javascript ES6, GraphQL, and SASS/CSS3.</p>
 	      		 <p>With a background in music composition and video art, I have spent the last four years as Front End Software Engineer, I've built pixel-perfect, responsive, WCAG Acessible
-	      		 websites for clients including <a href="https://www.juilliard.edu/" target="_blank">Juilliard</a>, <a href="http://www.virginia.edu/" target="_blank">University of Virginia</a>, <a href="https://gsas.columbia.edu/" target="_blank">Columbia GSAS</a>, <a href="https://www.sps.nyu.edu/" target="_blank">NYU SPS</a>, <a href="https://www.alvarezandmarsal.com/" target="_blank">Alvarez and Marsal</a>, and <a href="https://research.cornell.edu/" target="_blank">Cornell Research</a>.</p>
-	      		 <p>Recently I have immersed myself in full-stack Javascript programming, with an emphasis on React and Node.js. Please have a look at some of my personal React projects below, all the code can be found on my <a href="https://github.com/dgcohen" target="_blank">GitHub</a>.</p>
+	      		 websites for clients including <a href="https://www.juilliard.edu/" target="_blank" rel="noopener noreferrer">Juilliard</a>, <a href="http://www.virginia.edu/" target="_blank" rel="noopener noreferrer">University of Virginia</a>, <a href="https://gsas.columbia.edu/" target="_blank" rel="noopener noreferrer">Columbia GSAS</a>, <a href="https://www.sps.nyu.edu/" target="_blank" rel="noopener noreferrer">NYU SPS</a>, <a href="https://www.alvarezandmarsal.com/" target="_blank" rel="noopener noreferrer">Alvarez and Marsal</a>, and <a href="https://research.cornell.edu/" target="_blank" rel="noopener noreferrer">Cornell Research</a>.</p>
+	      		 <p>Recently I have immersed myself in full-stack Javascript programming, with an emphasis on React and Node.js. Please have a look at some of my personal React projects below, all the code can be found on my <a href="https://github.com/dgcohen" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
 	      		 <a className="contact" href="#contact">Get in touch</a>
 	      	</div>
 	      	<div className="image"></div>
@@ -53,21 +53,21 @@ class App extends Component {
 	      	<h3>Personal Projects</h3>
 	      	<ul className="projects">
             <li>
-              <a href="https://chess-react.herokuapp.com/" target="_blank"><img src={require('./assets/img/chess.jpg')} /></a>
-	      			<h4><a href="https://chess-react.herokuapp.com/" target="_blank">React Chess</a></h4>
-              <div className="links"><a href="https://chess-react.herokuapp.com/" target="_blank">Live Demo</a><a href="https://github.com/dgcohen/react-chess" target="_blank">GitHub</a></div>
+              <a href="https://chess-react.herokuapp.com/" target="_blank" rel="noopener noreferrer"><img alt="React Chess" src={require('./assets/img/chess.jpg')} /></a>
+	      			<h4><a href="https://chess-react.herokuapp.com/" target="_blank" rel="noopener noreferrer">React Chess</a></h4>
+              <div className="links"><a href="https://chess-react.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live Demo</a><a href="https://github.com/dgcohen/react-chess" target="_blank" rel="noopener noreferrer">GitHub</a></div>
 	      			<p>A single page chess app using a custom object-oriented chess engine built in React and custom SCSS.</p>
 	      		</li>
 	      		<li>
-              <a href="http://asteroids-js.herokuapp.com/" target="_blank"><img src={require('./assets/img/asteroids.jpg')} /></a>
-	      			<h4><a href="http://asteroids-js.herokuapp.com/" target="_blank">Asteroids.js</a></h4>
-              <div className="links"><a href="http://asteroids-js.herokuapp.com/" target="_blank">Live Demo</a><a href="https://github.com/dgcohen/asteroids-js" target="_blank">GitHub</a></div>
+              <a href="http://asteroids-js.herokuapp.com/" target="_blank" rel="noopener noreferrer"><img alt="Asteroids.js" src={require('./assets/img/asteroids.jpg')} /></a>
+	      			<h4><a href="http://asteroids-js.herokuapp.com/" target="_blank" rel="noopener noreferrer">Asteroids.js</a></h4>
+              <div className="links"><a href="http://asteroids-js.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live Demo</a><a href="https://github.com/dgcohen/asteroids-js" target="_blank" rel="noopener noreferrer">GitHub</a></div>
 	      			<p>An asteroids game built with prototype-based OOP Javascript. HTML5 Canvas used for live-rendering.</p>
 	      		</li>
 	      		<li>
-              <a href="http://callstack-exceeded.herokuapp.com" target="_blank"><img src={require('./assets/img/callstack.jpg')} /></a>
-	      			<h4><a href="http://callstack-exceeded.herokuapp.com" target="_blank">Callstack Exceeded</a></h4>
-              <div className="links"><a href="http://callstack-exceeded.herokuapp.com" target="_blank">Live Demo</a><a href="https://github.com/dgcohen/Callstack-Exceeded" target="_blank">GitHub</a></div>
+              <a href="http://callstack-exceeded.herokuapp.com" target="_blank" rel="noopener noreferrer"><img alt="Callstack Exceeded" src={require('./assets/img/callstack.jpg')} /></a>
+	      			<h4><a href="http://callstack-exceeded.herokuapp.com" target="_blank" rel="noopener noreferrer">Callstack Exceeded</a></h4>
+              <div className="links"><a href="http://callstack-exceeded.herokuapp.com" target="_blank" rel="noopener noreferrer">Live Demo</a><a href="https://github.com/dgcohen/Callstack-Exceeded" target="_blank" rel="noopener noreferrer">GitHub</a></div>
 	      			<p>A question and answer app for programmers based on Stack Overflow built with Rails 4 back-end with a custom RESTful API and Javascript and Backbone.js front-end for live-updating features.</p>
 	      		</li>
 	      	</ul>
